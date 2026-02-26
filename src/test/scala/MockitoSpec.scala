4260d4cce31a6e41fb63f301361c0edb80d836e2
zjjfly <zjjblue@126.com>
2026-02-26 20:33:37 +0800

    add type annotations

--- src/test/scala/MockitoSpec.scala
+++ src/test/scala/MockitoSpec.scala
@@ -1,7 +1,10 @@
 package com.github.zjjfly
 
+import org.specs2.matcher.MatchResult
 import org.specs2.mock._
 import org.specs2.mutable._
+
+import java.util
 
 /**
   * @author Zi JunJie[https://github.com/zjjfly]
@@ -18,16 +21,16 @@ A java list can be mocked
 """
 
   case class c() {
-    val m = mock[java.util.List[String]] // a concrete class would be mocked with: mock[new java.util.LinkedList[String]]
-    def stub = {
+    val m: util.List[String] = mock[java.util.List[String]] // a concrete class would be mocked with: mock[new java.util.LinkedList[String]]
+    def stub: MatchResult[Any] = {
       m.get(0) returns "one" // stub a method call with a return value
       m.get(0) must_== "one" // call the method
     }
-    def verify = {
+    def verify: MatchResult[String] = {
       m.get(0) returns "one" // stub a method call with a return value
       m.get(0) // call the method
       there was one(m).get(0) // verify that the call happened
     }
-    def verify2 = there was no(m).get(0) // verify that the call never happened
+    def verify2: MatchResult[String] = there was no(m).get(0) // verify that the call never happened
   }
 }
